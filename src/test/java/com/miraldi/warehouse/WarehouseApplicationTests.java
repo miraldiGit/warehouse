6c10e796b6ccd0222d886eab87656c9b572ee149
Miraldi Fifo <miraldi.fifo@ikubinfo.al>
2024-08-20 20:51:01 +0200

    Initial Commit

---
+++ src/test/java/com/miraldi/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraldi.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
